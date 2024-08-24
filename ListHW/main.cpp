85a15c765dd9599cd6bd3cbbb0eb871388e7f339
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-08-24 11:24:11 -0400

    ListHW DONE

--- ListHW/main.cpp
+++ ListHW/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 using namespace std;
 
 #define tab "\t"
@@ -104,7 +104,7 @@ using namespace std;
 		setlocale(LC_ALL, "");
 
 		int n;
-		cout << "Ââåäèòå êîëè÷åñòâî ýëåìåíòîâ: "; cin >> n;
+		cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¾Ð²: "; cin >> n;
 
 		List l1;
 		for (int i = 0; i < n; i++)
